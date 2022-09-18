6f7601000a0fb9c1a9bca849e807f209ad94e03b
LuanAnders0n <luan.anderson.001@gmail.com>
2022-09-17 22:24:53 -0300

    title

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,18 +3,18 @@ import { Container } from './styles'
 
 interface CardProps {
   img: any
-  tittle: string
+  title: string
   text?: string
 }
 
 const Card = (props: CardProps) => {
-  const { img, tittle, text } = props
+  const { img, title, text } = props
 
   return (
     <Container>
       <div>
         <img src={img} alt="cardLogo.png" />
-        <h1>{tittle}</h1>
+        <h1>{title}</h1>
         {text && <p>{text}</p>}
       </div>
     </Container>
