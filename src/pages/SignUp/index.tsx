5895dc9284b743e01d0e04198a12c3c5dcc36a03
Gabriel Tanaca <69495014+gabrieltanaca@users.noreply.github.com>
2022-09-28 18:35:07 -0300

    fix-form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 
-import { Container, StyledForm } from './styles';
+import { Container } from './styles';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
 interface Values {
@@ -14,78 +14,77 @@ interface Values {
 }
 
 const SignUp = () => {
+  const initialValues: Values = {
+    avatar: '',
+    firstName: '',
+    lastName: '',
+    nickname: '',
+    email: '',
+    password: '',
+  };
+
+  const onSubmit = (
+    values: Values,
+    { setSubmitting }: FormikHelpers<Values>
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  };
+
   return (
     <Container>
       <div className="image" />
-      <StyledForm>
-        <h1>Signup</h1>
-        <Formik
-          initialValues={{
-            avatar: '',
-            firstName: '',
-            lastName: '',
-            nickname: '',
-            email: '',
-            password: '',
-          }}
-          onSubmit={(
-            values: Values,
-            { setSubmitting }: FormikHelpers<Values>
-          ) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 500);
-          }}
-        >
-          <Form>
-            <Grid container>
-              <Grid xs={12}>
-                <label htmlFor="firstName">Avatar</label>
-                <Field id="avatar" name="avatar" type="file" accept="image/*" />
-              </Grid>
-
-              <Grid xs={6}>
-                <label htmlFor="firstName">Primeiro nome</label>
-                <Field id="firstName" name="firstName" />
-              </Grid>
-
-              <Grid xs={6}>
-                <label htmlFor="lastName">Ultimo nome</label>
-                <Field id="lastName" name="lastName" />
-              </Grid>
-
-              <Grid xs={12}>
-                <label htmlFor="nickname">Nickname</label>
-                <Field id="nickname" name="nickname" />
-              </Grid>
-
-              <Grid xs={12}>
-                <label htmlFor="email">Email</label>
-                <Field id="email" name="email" type="email" />
-              </Grid>
-
-              <Grid xs={12}>
-                <label htmlFor="password">Password</label>
-                <Field id="password" name="password" type="password" />
-              </Grid>
-
-              <Grid xs={12}>
-                <label htmlFor="password">Confirm Password</label>
-                <Field
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                />
-              </Grid>
-
-              <Grid xs={12}>
-                <button type="submit">Submit</button>
-              </Grid>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form className="form">
+            <h1>Signup</h1>
+          <Grid container spacing={2}>
+            <Grid xs={12}>
+              <label htmlFor="firstName">Avatar</label>
+              <Field id="avatar" name="avatar" type="file" accept="image/*" />
             </Grid>
-          </Form>
-        </Formik>
-      </StyledForm>
+
+            <Grid xs={6}>
+              <label htmlFor="firstName">Primeiro nome</label>
+              <Field id="firstName" name="firstName" />
+            </Grid>
+
+            <Grid xs={6}>
+              <label htmlFor="lastName">Ultimo nome</label>
+              <Field id="lastName" name="lastName" />
+            </Grid>
+
+            <Grid xs={12}>
+              <label htmlFor="nickname">Nickname</label>
+              <Field id="nickname" name="nickname" />
+            </Grid>
+
+            <Grid xs={12}>
+              <label htmlFor="email">Email</label>
+              <Field id="email" name="email" type="email" />
+            </Grid>
+
+            <Grid xs={12}>
+              <label htmlFor="password">Password</label>
+              <Field id="password" name="password" type="password" />
+            </Grid>
+
+            <Grid xs={12}>
+              <label htmlFor="password">Confirm Password</label>
+              <Field
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+              />
+            </Grid>
+
+            <Grid xs={12}>
+              <button type="submit">Submit</button>
+            </Grid>
+          </Grid>
+        </Form>
+      </Formik>
     </Container>
   );
 };
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -2,36 +2,32 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: flex-start;
   width: 100vw;
   height: 100vh;
 
   .image {
     width: 100%;
     height: 100%;
-    background-color: blue;
     position: absolute;
   }
-`;
 
-export const StyledForm = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 400px;
-  height: 100%;
-  z-index: 99;
-
-  /* .mb-3 {
+  .form {
     display: flex;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
-  } */
 
-  input {
-    background-color: #ffffff;
-    border-radius: 8px;
-    padding: 2px;
-    border: none;
-    display: flex;
+    width: 20%;
+    z-index: 2;
+
+    input {
+      display: flex;
+      background-color: #ffffff;
+      border-radius: 8px;
+      padding: 2px;
+      border: none;
+      width:100%;
+    }
   }
 `;
